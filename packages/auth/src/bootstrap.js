53e9d39ce69ff6ac895ba6d861695896396587d0
Kasperi Karjalainen <kasperi.karjalainen@viidakko.fi>
2022-05-04 08:52:55 +0300

    added auth project stuff

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
-
 import App from './App';
 
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
@@ -28,12 +28,16 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   };
 };
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
